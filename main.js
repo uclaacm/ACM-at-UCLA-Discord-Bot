76c3975bb8aab17db4d73e27931698ece323afc4
Rohit Nema <entrohpy@icloud.com>
2020-10-06 20:45:33 +0530

    Add stats command

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const sgMail = require('@sendgrid/mail');
 const sqlite = require('sqlite');
 const sqlite3 = require('sqlite3');
+const AsciiTable = require('ascii-table');
 const config = require('./config');
 
 // discord
@@ -772,6 +773,232 @@ WHERE
   ];
 }
 
+/* STATS FUNCTIONS */
+
+// get number of verified users
+// linked to STATS command
+async function getNumVerifiedStats() {
+  // open db
+  let db = await sqlite.open({
+    filename: config.db_path,
+    driver: sqlite3.Database,
+  });
+
+  // get count of users in table
+  let row = null;
+  try {
+    row = await db.get(
+      `
+SELECT
+  count(*) 'count'
+FROM users`
+    );
+  } catch (e) {
+    console.error(e.toString());
+    await db.close();
+    return [{ message: e.toString() }, null];
+  }
+  if (!row) {
+    return [
+      null,
+      `
+No stats available on verified users.`,
+    ];
+  }
+
+  return [
+    null,
+    `
+\`\`\`
+Number of Verified Users: ${row.count}
+\`\`\`
+    `
+  ];
+}
+
+// get count of each major
+// linked to STATS command
+async function getMajorStats() {
+  // open db
+  let db = await sqlite.open({
+    filename: config.db_path,
+    driver: sqlite3.Database,
+  });
+
+  // get count of each major
+  let rows = null;
+  try {
+    rows = await db.all(
+      `
+SELECT
+  major, count(*) 'count'
+FROM users
+GROUP BY major
+ORDER BY count DESC`
+    );
+  } catch (e) {
+    console.error(e.toString());
+    await db.close();
+    return [{ message: e.toString() }, null];
+  }
+  if (!rows) {
+    return [
+      null,
+      `
+No stats available on majors.`,
+    ];
+  }
+
+  let majorTable = new AsciiTable('Majors by Count');
+  majorTable.setHeading('Major','Count');
+  rows.forEach(row => majorTable.addRow(row.major, row.count));
+
+  return [
+    null,
+    `
+\`\`\`
+${majorTable.toString()}
+\`\`\`
+    `
+  ];
+}
+
+// get count of each graduation year
+// linked to STATS command
+async function getYearStats() {
+  // open db
+  let db = await sqlite.open({
+    filename: config.db_path,
+    driver: sqlite3.Database,
+  });
+
+  // get count of each year
+  let rows = null;
+  try {
+    rows = await db.all(
+      `
+SELECT
+  grad_year, count(*) 'count'
+FROM users
+GROUP BY grad_year
+ORDER BY count DESC`
+    );
+  } catch (e) {
+    console.error(e.toString());
+    await db.close();
+    return [{ message: e.toString() }, null];
+  }
+  if (!rows) {
+    return [
+      null,
+      `
+No stats available on graduation years.`,
+    ];
+  }
+
+  let yearTable = new AsciiTable('Graduation Year by Count');
+  yearTable.setHeading('Year','Count');
+  rows.forEach(row => yearTable.addRow(row.grad_year, row.count));
+
+  return [
+    null,
+    `
+\`\`\`
+${yearTable.toString()}
+\`\`\`
+    `
+  ];
+}
+
+// get number of transfers
+// linked to STATS command
+async function getNumTransferStats() {
+  // open db
+  let db = await sqlite.open({
+    filename: config.db_path,
+    driver: sqlite3.Database,
+  });
+
+  // get number of transfer students
+  let row = null;
+  try {
+    row = await db.get(
+      `
+SELECT
+  count(*) 'count'
+FROM users
+WHERE transfer_flag = 1`
+    );
+  } catch (e) {
+    console.error(e.toString());
+    await db.close();
+    return [{ message: e.toString() }, null];
+  }
+  if (!row) {
+    return [
+      null,
+      `
+No stats available on transfer students.`,
+    ];
+  }
+
+  return [
+    null,
+    `
+\`\`\`
+Number of Transfer Students: ${row.count}
+\`\`\`
+    `
+  ];
+}
+
+// get count of each affiliation
+// linked to STATS command
+async function getAffiliationStats() {
+  // open db
+  let db = await sqlite.open({
+    filename: config.db_path,
+    driver: sqlite3.Database,
+  });
+
+  // get count of each affiliation
+  let rows = null;
+  try {
+    rows = await db.all(
+      `
+SELECT
+  affiliation, count(*) 'count'
+FROM users
+GROUP BY affiliation
+ORDER BY count DESC`
+    );
+  } catch (e) {
+    console.error(e.toString());
+    await db.close();
+    return [{ message: e.toString() }, null];
+  }
+  if (!rows) {
+    return [
+      null,
+      `
+No stats available on affiliation.`,
+    ];
+  }
+
+  let affilTable = new AsciiTable('Affiliation by Count');
+  affilTable.setHeading('Affiliation','Count');
+  rows.forEach(row => affilTable.addRow(row.affiliation, row.count));
+
+  return [
+    null,
+    `
+\`\`\`
+${affilTable.toString()}
+\`\`\`
+    `
+  ];
+}
+
 // on ready, create db and tables if they don't already exist
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -1052,6 +1279,37 @@ client.on('message', async (msg) => {
     let userid = args[0];
     let nickname = args.slice(1).join(' ');
     [err, message] = await updateUserNickname(userid, nickname);
+  }
+
+  // stats: [ADMIN/MOD] get various stats on verified users
+  else if (command === 'stats' && (member.hasPermission('ADMINISTRATOR') || member.roles.cache.has(mod_role.id))) {
+    if (args.length < 1) {
+      msg.reply(
+        'Invalid command format. Format: `!stats (verified|major|year|transfer|affiliation)`'
+      );
+      return;
+    }
+
+    let option = args[0].toLowerCase();
+    switch (option) {
+      case 'verified': // number of verified users
+        [err, message] = await getNumVerifiedStats();
+        break;
+      case 'major': // breakdown of majors by count
+        [err, message] = await getMajorStats();
+        break;
+      case 'year': // breakdown of graduation year by count
+        [err, message] = await getYearStats();
+        break;
+      case 'transfer': // number of transfer students
+        [err, message] = await getNumTransferStats();
+        break;
+      case 'affiliation': // breakdown of affiliation by count
+        [err, message] = await getAffiliationStats();
+        break;
+      default:
+        message = 'Please enter a valid stat type (verified|major|year|transfer|affiliation)';
+    }
   }
 
   else if (command === 'help') {
