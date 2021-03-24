009f198fd488c2221286345ecd382fd23b4d14d5
brett <b.woltz123@gmail.com>
2021-03-23 20:47:05 -0700

    Resolve issue #8, split commands into seperate files

---
+++ commands/getUser.js
@@ -0,0 +1,147 @@
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
+const config = require('../config');
+
+// create user info message using Discord Message Embed for better formatting
+async function createUserInfoMsg(row, title, description, server, Discord) {
+    let member = await server.members.fetch(row.userid);
+
+    const userInfoEmbed = new Discord.MessageEmbed()
+    .setColor('#1e6cff')
+    .setTitle(title)
+    .setDescription(description)
+    .setThumbnail(member.user.avatarURL())
+    .addFields(
+        { name: 'Username', value: `${row.username}#${row.discriminator}`, inline: true },
+        { name: 'Nickname', value: row.nickname, inline: true },
+        { name: 'Pronouns', value: row.pronouns || '*not set*', inline: true },
+    )
+    .addFields(
+        { name: 'Major', value: row.major || '*not set*', inline: true },
+        { name: 'Year', value: row.grad_year || '*not set*', inline: true },
+        { name: 'Transfer?', value: (row.transfer_flag == 1 ? 'yes' : 'no'), inline: true },
+    )
+    .addFields(
+        { name: 'Affiliation', value: row.affiliation || '*not set*', inline: true },
+        { name: 'Email', value: row.email, inline: true },
+        { name: 'Verified at', value: row.verified_at + ' UTC', inline: true },
+    );
+
+    return userInfoEmbed;
+}
+
+module.exports = {
+    // who are you???
+    // linked to WHOAMI command
+    "whoami" : async function (userid, server, Discord) {
+        // open db
+        let db = await sqlite.open({
+        filename: config.db_path,
+        driver: sqlite3.Database,
+        });
+    
+        // check if user is verified
+        let row = null;
+        try {
+        row = await db.get(
+            `
+    SELECT
+        *
+    FROM users
+    WHERE
+        userid = ?`,
+            [userid]
+        );
+        } catch (e) {
+        console.error(e.toString());
+        await db.close();
+        return [{ message: e.toString() }, null];
+        }
+        await db.close();
+        if (!row) {
+        return [
+            null,
+            `
+    Hmmm I'm really not sure myself but I'd love to get to know you!
+    Use \`!iam <affiliation> <name> <ucla_email>\` and verify your email address.`,
+        ];
+        }
+    
+        return [
+        null,
+        await createUserInfoMsg(row, 'About You', `Why, you're ${row.nickname} of course!`, server, Discord)
+        ];
+    },
+
+    // get information on a user by discord username (note: users can change this)
+    // only `userid` is invariant. Use getUserById
+    // linked to LOOKUP command
+    "getUserByUsername" : async function (username, discriminator, server, Discord) {
+        // open db
+        let db = await sqlite.open({
+        filename: config.db_path,
+        driver: sqlite3.Database,
+        });
+    
+        // check if user is verified
+        let row = null;
+        try {
+        row = await db.get(
+            `
+    SELECT
+        *
+    FROM users
+    WHERE
+        username = ? AND
+        discriminator = ?`,
+            [username, discriminator]
+        );
+        } catch (e) {
+        console.error(e.toString());
+        await db.close();
+        return [{ message: e.toString() }, null];
+        }
+        await db.close();
+    
+        if (!row) {
+        return [null, 'User not found/verified.'];
+        }
+    
+        return [null, await createUserInfoMsg(row, 'User Information', `Moderator Lookup on ${row.userid}`, server, Discord)];
+    },
+
+    // get information on a user by discord username (note: users can change this)
+    // linked to LOOKUP command
+    "getUserById" : async function (userid, server, Discord) {
+        // open db
+        let db = await sqlite.open({
+        filename: config.db_path,
+        driver: sqlite3.Database,
+        });
+    
+        // check is user is verified
+        let row = null;
+        try {
+        row = await db.get(
+            `
+    SELECT
+        *
+    FROM users
+    WHERE
+        userid = ?`,
+            [userid]
+        );
+        } catch (e) {
+        console.error(e.toString());
+        await db.close();
+        return [{ message: e.toString() }, null];
+        }
+        await db.close();
+    
+        if (!row) {
+        return [null, 'User not found/verified.'];
+        }
+    
+        return [null, await createUserMessage.createUserInfoMsg(row, 'User Information', `Moderator Lookup on ${row.userid}`, server, Discord)];
+    }
+}
---
+++ commands/verify.js
@@ -0,0 +1,137 @@
+const sqlite = require('sqlite');
+const sqlite3 = require('sqlite3');
+const config = require('../config');
+
+const isModOrAdmin = (member, mod_role) =>
+member.hasPermission('ADMINISTRATOR') ||
+  member.roles.cache.has(mod_role.id);
+
+// verify code and and role to access server
+// linked to VERIFY command
+module.exports = {
+  "verify" : async function (code, author, server, verified_role, mod_role, alumni_role) {
+      // open db
+      let db = await sqlite.open({
+        filename: config.db_path,
+        driver: sqlite3.Database,
+      });
+    
+      // get member from the ACM server
+      let member = await server.members.fetch(author.id);
+    
+      // get iam details from usercodes table
+      let row = null;
+      let row_user = null;
+      try {
+        row = await db.get(
+          `
+    SELECT
+      email, nickname, affiliation
+    FROM usercodes
+    WHERE
+      userid = ? AND
+      code = ? AND
+      expires_at > datetime('now')`,
+          [author.id, code]
+        );
+        row_user = await db.get(
+          `
+    SELECT
+      pronouns
+    FROM users
+    WHERE
+      userid = ?`,
+          [author.id]
+        );
+      } catch (e) {
+        console.error(e.toString());
+        await db.close();
+        return [{ message: e.toString() }, null];
+      }
+      if (!row) {
+        await db.close();
+        return [null, 'Sorry, this code is either invalid/expired.'];
+      }
+    
+      // add verified role to user
+      await member.roles.add(verified_role);
+      if (row.affiliation === 'alumni') { // and if alumni, add alumni role
+        await member.roles.add(alumni_role);
+      }
+    
+      // set nickname: <name> (<pronouns>)
+      member.setNickname(row.nickname + (row_user ? ` (${row_user.pronouns})`: ''));
+    
+      try {
+        // delete usercode entry
+        await db.run('DELETE FROM usercodes WHERE userid = ?', [author.id]);
+    
+        // check if email is already verified
+        // it's possible for two users to request a code on the same email
+        // until any of the users has actually used the code!
+        let emailExists = null;
+        // TODO: treat .*.ucla.edu the same as ucla.edu for existence check
+        emailExists = await db.get('SELECT * FROM users WHERE email = ?', [row.email]);
+        if (emailExists) {
+          await db.close();
+          return [null, 'This email has already been verified. If you own this email address, please contact any of the Moderators.'];
+        }
+    
+        // add to users db (stores verified users)
+        await db.run(
+          `
+    INSERT INTO
+      users(userid, username, discriminator, nickname, email, affiliation)
+    VALUES
+      (?, ?, ?, ?, ?, ?)
+      ON CONFLICT(userid) DO
+      UPDATE
+      SET
+        username = ?,
+        discriminator = ?,
+        nickname = ?,
+        email = ?,
+        affiliation = ?`,
+          [
+            author.id,
+            author.username,
+            author.discriminator,
+            row.nickname,
+            row.email,
+            row.affiliation,
+            author.username,
+            author.discriminator,
+            row.nickname,
+            row.email,
+            row.affiliation
+          ]
+        );
+      } catch (e) {
+        console.error(e.toString());
+        await db.close();
+        return [{ message: e.toString() }, null];
+      }
+    
+      await db.close();
+      return [
+        null,
+        `Thanks ${row.nickname}! You have been verified and can now access the server! Please use the following commands to tell us a bit more about yourself!
+    \`\`\`
+    !major <valid_major>    | Your major
+    !transfer               | Transfer student
+    !year <grad_year>       | Your grad year
+    !pronouns <pronouns>    | Max 10 characters
+    !whoami                 | View your information
+    !help                   | Show all commands
+    \`\`\`
+    ` + (isModOrAdmin(member, mod_role) ? `
+    Since you're a Moderator, you can also use the following commands:
+    \`\`\`
+    !name <userid> <new_name>                          | change userids nickname
+    !lookup <userid>                                   | lookup verified user
+    !stats <verified|major|year|transfer|affiliation>  | Useful for analytics
+    \`\`\`
+    ` : '')
+      ];
+    }
+}
