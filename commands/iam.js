687ba72038bbb0d25b0f6cf7ca58104c7d069c24
Rohit Nema <entrohpy@icloud.com>
2021-09-25 02:42:03 -0700

    Add comments to explain email regex

--- commands/iam.js
+++ commands/iam.js
@@ -20,6 +20,9 @@ const iam = async function(userid, email, nickname, affiliation, sgMail) {
     return [null, 'Please provide a valid affiliation (student/alumni/other).'];
   }
 
+  // regex matches and captures main domain name (index 1) and TLD (index 2)
+  // regex conforms to RFC 5322
+  // https://stackoverflow.com/a/201378 (modified to capture necessary groups)
   let match_groups = email.match(
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:([a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\.)+([a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))/
   );
--- commands/verify.js
+++ commands/verify.js
@@ -52,6 +52,9 @@ const verify = async function(code, author, server, guest_role, verified_role, m
     return [null, 'Sorry, this code is either invalid/expired.'];
   }
 
+  // regex matches and captures main domain name (index 1) and TLD (index 2)
+  // regex conforms to RFC 5322
+  // https://stackoverflow.com/a/201378 (modified to capture necessary groups)
   let match_groups = row.email.match(
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:([a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\.)+([a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))/
   );
